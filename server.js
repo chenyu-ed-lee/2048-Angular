7a091f6bdf7a9eee6bb171c80e326f39efeeb8e4
Ed Lee <chenyu.ed.lee@gmail.com>
2016-04-02 13:03:53 -0700

    restructured

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.use(express.static(path.join(__dirname, './app')));
+// require('./config/mongoose.js');
+// var bodyParser = require('body-parser');
+// app.use(bodyParser.urlencoded());
+// app.use(bodyParser.json());
+// require('./config/route.js')(app);
+
+var port = process.env.PORT || 8000;
+console.log("Listening on " + port);
+
+app.listen(port);
